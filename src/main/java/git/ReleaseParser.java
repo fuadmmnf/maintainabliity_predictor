e4443efafa05750b0d789f698898c98ba4fc9699
Musa406 <bsse0824@iit.du.ac.bd>
2021-04-08 22:03:24 +0600

    finds all commit info based on their corresponding branch

--- src/main/java/git/ReleaseParser.java
+++ src/main/java/git/ReleaseParser.java
@@ -16,6 +16,7 @@ import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -32,67 +33,24 @@ public class ReleaseParser {
 
         Git git = new Git(repository);
 
-        RevWalk walk = new RevWalk(repository);
-
         List<Ref> branches = new Git(repository).branchList().setListMode(ListBranchCommand.ListMode.ALL).call();
 
+        int branchCount = 0;
         for (Ref branch : branches) {
-            String branchName = branch.getName();
 
-            System.out.println("Commits of branch: " + branch.getName());
+            System.out.println("Branch: " + branch + " " + branch.getName() + " "
+                    + branch.getObjectId().getName());
             System.out.println("-------------------------------------");
+            branchCount++;
 
             Iterable<RevCommit> commits = git.log().all().call();
 
             for (RevCommit commit : commits) {
-                boolean foundInThisBranch = false;
-
-                RevCommit targetCommit = walk.parseCommit(repository.resolve(
-                        commit.getName()));
-
-//                for (Map.Entry<String, Ref> e : repository.getAllRefs().entrySet()) {
-//                    if (e.getKey().startsWith(Constants.R_HEADS)) {
-//                        if (walk.isMergedInto(targetCommit, walk.parseCommit(
-//                                e.getValue().getObjectId()))) {
-//                            String foundInBranch = e.getValue().getName();
-//                            if (branchName.equals(foundInBranch)) {
-//                                foundInThisBranch = true;
-//                                break;
-//                            }
-//                        }
-//                    }
-//                }
-//
-//                if (foundInThisBranch) {
-//                    System.out.println(commit.getName());
-//                    System.out.println(commit.getAuthorIdent().getName());
-//                    System.out.println(new Date(commit.getCommitTime() * 1000L));
-//                    System.out.println(commit.getFullMessage());
-//                }
-//            }
+                    System.out.println(commit.getName());
+                    System.out.println(commit.getAuthorIdent().getName());
+                    System.out.println(new Date(commit.getCommitTime() * 1000L));
+                    System.out.println(commit.getFullMessage());
             }
         }
-
-//        System.out.println(repository.getRemoteNames());
-//        System.out.println(git.branchList().setListMode(ListBranchCommand.ListMode.ALL).call());
-//        ObjectId lastCommitId = repository.resolve(Constants.HEAD);
-//        System.out.println(lastCommitId);
-//        try (RevWalk revWalk = new RevWalk(repository)) {
-//            RevCommit commit = revWalk.parseCommit(lastCommitId);
-//            // and using commit's tree find the path
-//            RevTree tree = commit.getTree();
-//            System.out.println("Having tree: " + tree);
-//        }
-
-
-//        int c = 0;
-
-//        List<Ref> call = new Git(repository).branchList().setListMode(ListBranchCommand.ListMode.ALL).call();
-//        for (Ref ref : call) {
-//            System.out.println("Branch: " + ref + " " + ref.getName() + " "
-//                    + ref.getObjectId().getName());
-//            c++;
-//        }
-//        System.out.println("Number of branches: " + c);
     }
 }
