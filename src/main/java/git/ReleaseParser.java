fef401ac4ed2e056bcf1be32d7ca6508bdf36ad1
Musa406 <bsse0824@iit.du.ac.bd>
2021-04-10 09:32:58 +0600

    checkout to a specific tag

--- src/main/java/git/ReleaseParser.java
+++ src/main/java/git/ReleaseParser.java
@@ -1,5 +1,6 @@
 package git;
 
+import org.codehaus.plexus.util.Scanner;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.ListBranchCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
@@ -24,8 +25,11 @@ import java.util.Map;
 public class ReleaseParser {
     public static void main(String[] args) throws InvalidRemoteException, TransportException, GitAPIException, IOException {
 
+//        File myObj = new File("filename.txt");
+//        Scanner myReader = new Scanner(myObj);
+
         FileRepositoryBuilder repositoryBuilder = new FileRepositoryBuilder();
-        Repository repository = repositoryBuilder.setGitDir(new File("H:\\musa_all\\project\\spl3\\.git"))
+        Repository repository = repositoryBuilder.setGitDir(new File("H:\\ES_git\\elasticsearch\\.git"))
                 .readEnvironment() // scan environment GIT_* variables
                 .findGitDir() // scan up the file system tree
                 .setMustExist(true)
@@ -33,24 +37,37 @@ public class ReleaseParser {
 
         Git git = new Git(repository);
 
-        List<Ref> branches = new Git(repository).branchList().setListMode(ListBranchCommand.ListMode.ALL).call();
+        git.checkout()
+                .setCreateBranch(false)
+                .setName("Elasticsearch")
+                .setStartPoint("refs/tags/v7.11.2")
+                .call();
 
-        int branchCount = 0;
-        for (Ref branch : branches) {
-
-            System.out.println("Branch: " + branch + " " + branch.getName() + " "
-                    + branch.getObjectId().getName());
-            System.out.println("-------------------------------------");
-            branchCount++;
-
-            Iterable<RevCommit> commits = git.log().all().call();
-
-            for (RevCommit commit : commits) {
-                    System.out.println(commit.getName());
-                    System.out.println(commit.getAuthorIdent().getName());
-                    System.out.println(new Date(commit.getCommitTime() * 1000L));
-                    System.out.println(commit.getFullMessage());
-            }
-        }
+//        List<Ref> tags = git.tagList().call();
+//
+//        System.out.println(git.tagList().call());
+//        for (Ref tag : tags) {
+//            System.out.println("Tag: " + tag.getName());
+//        }
+//
+//        List<Ref> branches = new Git(repository).branchList().setListMode(ListBranchCommand.ListMode.ALL).call();
+//
+//        int branchCount = 0;
+//        for (Ref branch : branches) {
+//
+//            System.out.println("Branch: " + branch + " " + branch.getName() + " "
+//                    + branch.getObjectId().getName());
+//            System.out.println("-------------------------------------");
+//            branchCount++;
+//
+//            Iterable<RevCommit> commits = git.log().all().call();
+//
+//            for (RevCommit commit : commits) {
+//                    System.out.println(commit.getName());
+//                    System.out.println(commit.getAuthorIdent().getName());
+//                    System.out.println(new Date(commit.getCommitTime() * 1000L));
+//                    System.out.println(commit.getFullMessage());
+//            }
+//        }
     }
 }
