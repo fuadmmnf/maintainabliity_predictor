f41374a3a69b62058c453a24051bbee9cd46e600
fuadmmnf <bsse0920@iit.du.ac.bd>
2021-05-18 23:09:33 +0600

    tcc done

--- src/main/java/metrics/TCCCalculator.java
+++ src/main/java/metrics/TCCCalculator.java
@@ -4,19 +4,48 @@ import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.expr.FieldAccessExpr;
+import com.github.javaparser.ast.expr.VariableDeclarationExpr;
+import com.github.javaparser.ast.stmt.BlockStmt;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+import java.util.stream.Collectors;
 
 public class TCCCalculator {
-    public static void main(String[] args) {
-        CompilationUnit compilationUnit = StaticJavaParser.parse("test/TCCTest.java");
+
+    public static void main(String[] args) throws FileNotFoundException {
+        CompilationUnit compilationUnit = StaticJavaParser.parse(new File("src/main/java/metrics/test/TCCTest.java"));
         compilationUnit.accept(new ClassVisitor(), null);
     }
 
     private static class ClassVisitor extends VoidVisitorAdapter<Void> {
-        final Pattern pattern = Pattern.compile("^((private|public|protected)?\\s+)?.*\\s+(\\w+);$");
+        private final List<VariableDeclarator> attributes = new ArrayList<>();
+
+        private List<String> getScopeVariables(BlockStmt block) {
+            List<String> scopedVariables = new ArrayList<>();
+            block.findAll(VariableDeclarationExpr.class).forEach(field -> {
+                scopedVariables.addAll(field.getVariables().stream().map(f -> f.getName().asString()).collect(Collectors.toList()));
+            });
+
+            return scopedVariables;
+        }
+
+        private List<String> getUsedFieldsInMethod(BlockStmt block) {
+            List<String> usedFields = new ArrayList<>();
+            block.findAll(FieldAccessExpr.class).forEach(field -> {
+                usedFields.add(field.getName().asString());
+            });
+//            System.out.println(usedFields);
+            return usedFields;
+        }
 
         @Override
         public void visit(ClassOrInterfaceDeclaration n, Void arg) {
@@ -24,16 +53,55 @@ public class TCCCalculator {
          this method will be called for all methods in this
          CompilationUnit, including inner class methods */
             for (FieldDeclaration field : n.getFields()) {
-                // create a regex-matcher
-                final Matcher matcher = pattern.matcher(field.getVariable(0).getName().asString());
+                attributes.addAll(field.getVariables());
+            }
 
-                // if field matches regex
-                if (matcher.matches()) {
-                    // get the last group -> the fieldName
-                    final String name = matcher.group(matcher.groupCount());
-                    System.out.println("FieldName1: " + name);
+
+            List<MethodDeclaration> publicMethods = n.getMethods();
+
+//            for (MethodDeclaration method : publicMethods
+//            ) {
+////                System.out.println(method.getName());
+//                if (method.getBody().isEmpty())
+//                    continue;
+//                List<String> methodClassAttributesUsed = getUsedFieldsInMethod(method.getBody().get());
+//                System.out.println(methodClassAttributesUsed);
+//                List<String> methodClassAttributesUsed2 = (getScopeVariables(method.getBody().get()));
+//                System.out.println(methodClassAttributesUsed2);
+//            }
+
+
+            int directlyConnectedMethodCount = 0;
+            for (int i = 0; i < publicMethods.size(); i++) {
+                MethodDeclaration method1 = publicMethods.get(i);
+
+                if (!method1.asMethodDeclaration().getModifiers().get(0).getKeyword().asString().equals("public") || method1.getBody().isEmpty())
+                    continue;
+                List<String> method1ClassAttributesUsed = getUsedFieldsInMethod(method1.getBody().get());
+                method1ClassAttributesUsed.removeAll(getScopeVariables(method1.getBody().get()));
+
+
+                for (int j = i + 1; j < publicMethods.size(); j++) {
+                    MethodDeclaration method2 = publicMethods.get(j);
+                    if (!method2.asMethodDeclaration().getModifiers().get(0).getKeyword().asString().equals("public") || method2.getBody().isEmpty())
+                        continue;
+                    List<String> method2ClassAttributesUsed = getUsedFieldsInMethod(method2.getBody().get());
+                    method2ClassAttributesUsed.removeAll(getScopeVariables(method2.getBody().get()));
+
+
+                    boolean isTightCohesion = method1ClassAttributesUsed.stream()
+                            .anyMatch(
+                                    new HashSet<>(method2ClassAttributesUsed)::contains);
+                    if (isTightCohesion) {
+//                        System.out.println(method2ClassAttributesUsed);
+                        directlyConnectedMethodCount++;
+                    }
                 }
             }
+
+            double tcc = (directlyConnectedMethodCount) / ((publicMethods.size() * (publicMethods.size() - 1)) / 2.0);
+            System.out.println("Tight Class Cohesion: " + tcc);
+
             super.visit(n, arg);
         }
     }
--- src/main/java/metrics/test/TCCTest.java
+++ src/main/java/metrics/test/TCCTest.java
@@ -2,4 +2,21 @@ package metrics.test;
 
 public class TCCTest {
     private int a, b, c, d, e;
+    public void func1(int x){
+        int a, d, tweq, qwd;
+        a= 2;
+        this.b=4;
+        this.e=5;
+        d= 2;
+    }
+
+    public void func2(){
+        int k;
+        this.b=6;
+        k = 2;
+    }
+
+    public void func3(){
+
+    }
 }
